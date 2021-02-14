6d75eba4002355d7639f9aa0667a23f0625d720f
Benjamin Berkels <torr.samaho@quantentunnel.de>
2021-02-14 21:00:26 +0100

    Generalized the RingBuffer class to support data types that are classes with their own memory management, based on a patch by Adam Kaminski.

--- src/networkshared.h
+++ src/networkshared.h
@@ -549,17 +549,26 @@ public:
 template<typename DataType, int Length>
 class RingBuffer
 {
-	DataType _data[Length];
+	DataType* _data;
 	// Position of the entry that will be overwritten next AKA the oldest entry.
 	unsigned int _position;
 public:
-	RingBuffer ( )
+	RingBuffer ( ) : _data ( NULL )
 	{
 		clear();
 	}
+	~RingBuffer ( )
+	{
+		if ( _data != NULL )
+			delete[] _data;
+	}
 	void clear ( )
 	{
-		memset( _data, 0, sizeof( _data ));
+		if ( _data != NULL )
+			delete[] _data;
+
+		// Make sure to initialize also built-in types by appending "()".
+		_data = new DataType[Length]();
 		_position = 0;
 	}
 	void put ( DataType Entry )
